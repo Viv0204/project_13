5f91e8814d17c58d807dc8ff4dd6e49dd5a088d0
Ivia08 <100661098+Viv0204@users.noreply.github.com>
2022-03-25 23:25:58 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -65,25 +65,25 @@ function draw() {
   //   }
   // }
 
-   if (frameCount / 80 == 0) {
-     if (select_sprites == 1) {
-       createApples();
-     } else if (select_sprites == 2) {
-        createOrange();
+  // if (frameCount / 80 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //    createRed();
+  //   }
+  // }
+
+  if (frameCount % 80 = 0) {
+   if (select_sprites == 1) {
+      createApples();
+    } else if (select_sprites == 2) {
+      createOrange();
      }else {
-       createRed();
+      createRed();
      }
    }
-
-//   if (frameCount % 80 = 0) {
-//    if (select_sprites == 1) {
-//       createApples();
-//     } else if (select_sprites == 2) {
-//       createOrange();
-//     }else {
-//       createRed();
-//     }
-//   }
 
 
 
